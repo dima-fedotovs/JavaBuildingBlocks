12276e9b2391fb285947fdb071e2c2bb541ab6d8
Dimitrijs Fedotovs <dima@fedoto.ws>
2020-11-30 20:51:08 +0200

    Initial commit

---
+++ src/demo/WhileDemo.java
@@ -0,0 +1,17 @@
+package demo;
+
+import java.util.Scanner;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("do you want to start?");
+
+        while (scanner.next().equals("yes")) {
+            System.out.println("doing something");
+            System.out.println("do you want to repeat?");
+        }
+
+        System.out.println("Good bye");
+    }
+}
